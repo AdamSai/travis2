94cfd5dc276243582db9ce444ced895e4a6220c0
AdamSai <adamsaidane252@gmail.com>
2020-10-29 14:36:28 +0100

    test

---
+++ WcfService2Tests/Service1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfService2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService2.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetDataUsingDataContractTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
